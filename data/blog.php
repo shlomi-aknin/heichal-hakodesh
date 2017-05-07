32ead0df40950cf0f9fb9e4dc065b3108cd2cb6e
shlomi-aknin <shlomi.aknin@gmail.com>
2017-05-07 11:31:52 +0300

    fixed height of blog items, limited small text length, added gitignore

--- data/blog.php
+++ data/blog.php
@@ -12,6 +12,8 @@
 				$main_img_src = $data['dir']['uploads'].$main_img_date.'/'.$main_img->getName();
 			}
 			$link = 'single.php?cat='.$article->getCatId().'&id='.$article->getId();
+			$data['short_text'] = $article->getText();
+			if(strlen($data['short_text']) > 100) $data['short_text'] = mb_substr($data['short_text'], 0, 120) . '...';
 			$data['articles_html'] .= '<div class="col-lg-4 col-md-4 pull-right">';
 			$data['articles_html'] .= '<div class="fh5co-blog animate-box">';
 			$data['articles_html'] .= '<a href="'.$link.'"><img class="img-responsive" src="'.$main_img_src.'" alt=""></a>';
@@ -19,8 +21,8 @@
 			$data['articles_html'] .= '<h3><a href=""#>'.$article->getName().'</a></h3>';
 			$data['articles_html'] .= '<span class="posted_on">'.defDate($article->getDate()).'</span>';
 			$data['articles_html'] .= '<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>';
-			$data['articles_html'] .= '<p>'.$article->getText().'</p>';
-			$data['articles_html'] .= '<a href="'.$link.'" class="btn btn-primary">קרא עוד</a>';
+			$data['articles_html'] .= '<p>'.$data['short_text'].'</p>';
+			$data['articles_html'] .= '<div class="read-more-btn"><a href="'.$link.'" class="btn btn-primary">קרא עוד</a></div>';
 			$data['articles_html'] .= '</div></div></div>';
 		}
 	}
